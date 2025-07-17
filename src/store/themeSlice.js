bb7bd6a214b128d5b94b6ca0275dc40dcc40b83e
Zheneos <loic.scognamiglio16@gmail.com>
2025-07-17 22:24:38 +0200

    fix:Changement chargement couleur sur le compte

--- src/store/themeSlice.js
+++ src/store/themeSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialTheme = localStorage.getItem('theme') || 'dark'; // Récupère le mode enregistré ou par défaut 'dark'
+document.documentElement.style.setProperty("--jaune", localStorage.getItem('color'));
 switchColor(initialTheme)
 
 const themeSlice = createSlice({
@@ -47,18 +48,18 @@ function switchColor(theme) {
   document.documentElement.style.setProperty("--color-header-text", theme === 'dark' ? '#fff' : '#000');
 
   document.documentElement.style.setProperty("--color-backgroundBoard", theme === 'dark' ? '#444444' : '#fff');
-    document.documentElement.style.setProperty("--color-header", theme === 'dark' ? '#2e2e2e' : '#fff');
+  document.documentElement.style.setProperty("--color-header", theme === 'dark' ? '#2e2e2e' : '#fff');
   document.documentElement.style.setProperty("--color-hover", theme === 'dark' ? '#575757' : '#e9ecef');
 
   document.documentElement.style.setProperty("--color-input-bg", theme === 'dark' ? '#2c2c2c' : '#f3f3f3');
   document.documentElement.style.setProperty("--color-field-bg", theme === 'dark' ? '#2c2c2c' : '#f3f3f3');
 
+  document.documentElement.style.setProperty("--btn-hover-bg", theme === 'dark' ? '#3a3a3a' : '#f0f0f0');
+  document.documentElement.style.setProperty("--color-home-gradient", theme === 'dark' ? 'linear-gradient(135deg, #121212, #222)' : 'linear-gradient(135deg, #f5f5f5, #e0e0e0)');
+
   document.documentElement.style.setProperty("--color-cancel-bg", '#bdc3c7');
   document.documentElement.style.setProperty("--color-cancel-text", '#000');
 
-  document.documentElement.style.setProperty("--btn-hover-bg", theme === 'dark' ? '#3a3a3a' : '#f0f0f0');
-  document.documentElement.style.setProperty("--btn-danger", '#c0392b');
-  document.documentElement.style.setProperty("--color-home-gradient", theme === 'dark' ? 'linear-gradient(135deg, #121212, #222)' : 'linear-gradient(135deg, #f5f5f5, #e0e0e0)');
 }
 
 
